ccf35d872bca2d5375d6de45ee270c904999183b
aman <amanaj402@gmail.com>
2025-08-10 22:00:01 +0530

    simple code1

--- variables.tf
+++ variables.tf
@@ -1,21 +1,21 @@
-variable "rg_name" {}
-variable "rg_location" {}
-variable "account_replication_type" {}
-variable "account_tier"{}
-variable "name"{}
+# variable "rg_name" {}
+# variable "rg_location" {}
+# variable "account_replication_type" {}
+# variable "account_tier"{}
+# variable "name"{}
 
 
-resource "azurerm_resource_group" "amanrg10" {
- name     = var.rg_name
-location = var.rg_location
-}
+# resource "azurerm_resource_group" "amanrg10" {
+#  name     = var.rg_name
+# location = var.rg_location
+# }
 
-resource "azurerm_storage_account" "Ivanstorage112" {
-    depends_on = [azurerm_resource_group.amanrg10]
-  name                     = var.name
-  resource_group_name      = var.rg_name
-  location                 = var.rg_location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  }
+# # resource "azurerm_storage_account" "Ivanstorage112" {
+# #     depends_on = [azurerm_resource_group.amanrg10]
+# #   name                     = var.name
+# #   resource_group_name      = var.rg_name
+# #   location                 = var.rg_location
+# #   account_tier             = var.account_tier
+# #   account_replication_type = var.account_replication_type
+# #   }
 
