3ae09182af6c173867d4899bb333973a8d27c8e5
aman <amanaj402@gmail.com>
2025-08-10 22:31:41 +0530

    neha code

--- aman.tf
+++ aman.tf
@@ -10,6 +10,11 @@ location = "West Europe"
 }
 
 
+resource "azurerm_resource_group" "neharg101" {
+name     = "neha-rg2"
+location = "West Europe"
+}
+
 resource "azurerm_resource_group" "amanrg10" {
  name     = "ivan-rg"
  location = "West Europe"
